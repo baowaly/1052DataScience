9b588b88ca7c50ef751e4811971f4a30382563b2
baowaly <baowaly@gmail.com>
2017-03-01 01:34:11 +0800

    update homework

--- hw1_exam.R
+++ hw1_exam.R
@@ -1,11 +1,26 @@
 ########################
 # homework1 example
 ########################
-
 args = commandArgs(trailingOnly=TRUE)
-if (length(args)==0) {
+if (length(args) < 2) {
   stop("USAGE: Rscript hw1_exam.R input", call.=FALSE)
-} else if (length(args)==1) {
-  i_f <- args[1] 
+} else {
+  i_f <- args[1]
+  o_f <- args[2]
 }
-print(i_f)
+
+#print(i_f)
+#print(o_f)
+
+inputData <- read.csv(file=i_f, header=TRUE, sep=",")
+
+max_weight <- round(max(inputData$weight),2)
+max_height <- round(max(inputData$height),2)
+
+file_without_ext <- tools::file_path_sans_ext(basename(i_f))
+row <- cbind(set=file_without_ext, weight=max_weight, height=max_height)
+if(!file.exists(o_f)){
+  file.create(o_f)
+}
+
+write.csv(row, file=o_f, quote = FALSE, row.names = FALSE)
