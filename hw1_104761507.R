9f0beaeb358aaaaa745874501436b674ec302a0d
baowaly <baowaly@gmail.com>
2017-03-01 13:40:32 +0800

    rename script and update

---
+++ hw1_104761507.R
@@ -0,0 +1,41 @@
+########################
+# homework1 example
+########################
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args) < 4 ) {
+  stop("USAGE: Rscript hw1_104761507.R -files test.1.csv -out result.csv", call.=FALSE)
+} else {
+  
+  if(args[1]=="-files")
+    i_f <- args[2]
+  if(args[3]=="-out")
+    o_f <- args[4]
+  
+  if(args[1]=="-out")
+    o_f <- args[2]
+  if(args[3]=="-files")
+    i_f <- args[4]
+}
+
+if(!file.exists(i_f)){
+  stop("Input file does not exist!!", call.=FALSE)
+}
+
+if(length(o_f)==0){
+  stop("No Output file found!!", call.=FALSE)
+}
+
+inputData <- read.csv(file=i_f, header=TRUE, sep=",")
+max_weight <- round(max(inputData$weight),2)
+max_height <- round(max(inputData$height),2)
+
+file_without_ext <- tools::file_path_sans_ext(basename(i_f))
+row <- cbind(set=file_without_ext, weight=max_weight, height=max_height)
+if(!file.exists(o_f)){
+  file.create(o_f)
+}
+
+write.csv(row, file=o_f, quote = FALSE, row.names = FALSE)
+
+#Rscript hw1_104761507.R -files test.1.csv -out result.csv 
